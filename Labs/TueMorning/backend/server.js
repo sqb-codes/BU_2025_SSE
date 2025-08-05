a95eb521940b6e79d17d97034e0edbc890c10b27
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-08-05 09:31:45 +0530

    Add files via upload

--- Labs/TueMorning/backend/server.js
+++ Labs/TueMorning/backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1>');
+  res.send('<h1>Hello, World with Docker + Express!!!</h1>');
 });
 
 app.listen(port, () => {
