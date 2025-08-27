325172bbd31a9c9208ffa579529255c19b1cbe02
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2025-08-27 13:21:10 +0530

    Add files via upload

--- Theory/microservices-ecommerce/product-service/server.js
+++ Theory/microservices-ecommerce/product-service/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 1234;
+const {PORT} = require('./utils/config');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/authRoutes');
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/producrs', productRoutes);
+
 
 app.listen(PORT, () => {
-  console.log(`User service is running on port ${PORT}`);
+  console.log(`Product service is running on port ${PORT}`);
 });
