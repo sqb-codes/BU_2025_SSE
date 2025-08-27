6dac382251368d919fc6ced311e7c35f48741016
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2025-08-27 09:18:09 +0530

    Add files via upload

--- Labs/WedMorning/backend-app/server.js
+++ Labs/WedMorning/backend-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World with Docker + Express...</h1>');
+  res.send('<h1>Hello, World with Docker + Express in prod env...</h1>');
 });
 
 const PORT = process.env.PORT || 3000;
