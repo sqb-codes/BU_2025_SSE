3a85c4180d14d8cae23c9365d9da67b7ad0f06f8
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2025-09-10 21:44:37 +0530

    Add files via upload

--- Theory/microservices-ecommerce/product-service/routes/productRoutes.js
+++ Theory/microservices-ecommerce/product-service/routes/productRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, getAllProducts, getProductById } = require('../controller/product');
+const { addProduct, getAllProducts, getProductById } = require('../controller/productController');
 
-router.post('/products', addProduct);
-router.get('/products', getAllProducts);
-router.get('/products/:id', getProductById);
+router.post('/addProduct', addProduct);
+router.get('/getProducts', getAllProducts);
+router.get('/getProduct/:id', getProductById);
 
 module.exports = router;
--- Theory/microservices-ecommerce/product-service/server.js
+++ Theory/microservices-ecommerce/product-service/server.js
@@ -5,14 +5,33 @@ dotenv.config();
 const app = express();
 const {PORT} = require('./utils/config');
 const connectDB = require('./config/db');
-const productRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const {connectRabbitMQ, consumeMessage} = require('./utils/rabbitMQ');
 
 connectDB();
 
 app.use(express.json());
 
-app.use('/api/producrs', productRoutes);
+app.use('/api/products', productRoutes);
 
+async function processOrder(order) {
+  console.log("Product service received order:", order);
+  for(let item of order.products) {
+    const product = await Product.findById(item.productId);
+    if(!product) {
+      continue;
+    }
+    product.stock -= item.quantity;
+    await product.save();
+    console.log(`Updated stock for product ${product.name}: ${product.stock}`);
+  }
+}
+
+// IIFE to setup RabbitMQ connection and start consuming messages
+(async () => {
+  await connectRabbitMQ();
+  await consumeMessage('orderQueue', processOrder);
+})();
 
 app.listen(PORT, () => {
   console.log(`Product service is running on port ${PORT}`);
